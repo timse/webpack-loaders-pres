87076fb7e80188a7edc88810eb126a50efa8bad2
Tim Sebastian <tsebastian@atlassian.com>
2017-01-19 09:34:05 +1100

    introduce placeholder image

--- imgloader.js
+++ imgloader.js
@@ -1,12 +1,17 @@
+const path = require('path');
+
 const loader = function(content) {
   return content;
 }
 loader.raw = true;
 
 loader.pitch = function(remainingRequest) {
+  const resource = this.resourcePath;
+  const placeholderPath = path.join(__dirname, 'placeholder.js');
   return `
+    var placeholder = require('!!${placeholderPath}!${resource}');
     var path = require('!${remainingRequest}');
-    module.exports = '<img src="' + path + '"/>'
+    module.exports = '<img src="' + placeholder.url + '" id="image" /><script>var i = new Image();i.src="' + path + '";i.onload=function(){var oi =document.querySelector("#image"); oi.parentElement.replaceChild(i,oi);}</script>'
   `;
 }
 
