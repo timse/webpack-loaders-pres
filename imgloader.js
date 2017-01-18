e3073817479cbc14810ba52b07a4011345e4ed1b
Tim Sebastian <tim.sebastian@gmail.com>
2017-01-18 22:19:43 +1100

    back in the game with additional requests

--- imgloader.js
+++ imgloader.js
@@ -1,6 +1,13 @@
 const loader = function(content) {
-  return `module.exports = '<img src="${content}"/>'`;
+  return content;
 }
 loader.raw = true;
+
+loader.pitch = function(remainingRequest) {
+  return `
+    var path = require('!${remainingRequest}');
+    module.exports = '<img src="' + path + '"/>'
+  `;
+}
 
 module.exports = loader;
